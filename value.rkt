be07ad279c2e2a2e02a9431d9b7d4513de1dce25
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-17 16:56:53 +0800

    llvm values helper

---
+++ value.rkt
@@ -0,0 +1,10 @@
+#lang racket/base
+
+(provide (all-defined-out))
+
+(require ffi/unsafe
+         "definer.rkt")
+
+(define-llvm llvm-constant? (_fun _LLVMValueRef -> _bool) #:c-id LLVMIsConstant)
+(define-llvm llvm-undef? (_fun _LLVMValueRef -> _bool) #:c-id LLVMIsUndef)
+(define-llvm llvm-poison? (_fun _LLVMValueRef -> _bool) #:c-id LLVMIsPoison)
