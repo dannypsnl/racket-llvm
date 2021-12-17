270a2ee58f0a798184baccf1bde4689536385c95
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-17 20:41:48 +0800

    info

--- info.rkt
+++ info.rkt
@@ -1,8 +1,10 @@
 #lang info
 (define collection "racket-llvm13")
 (define deps '("base"))
-(define build-deps '("rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "LLVM 13 bindings for Racket")
 (define version "0.0")
-(define pkg-authors '(Luke Lau))
+(define license '(Apache-2.0 OR MIT))
+(define pkg-authors '(dannypsnl))
+
 (define test-omit-paths '("examples"))
