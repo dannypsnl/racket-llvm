59d0f9b975264686f1fd0e71eabf292445ce717a
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-17 16:34:38 +0800

    more types
    * full integer
    * every float variants

--- types.rkt
+++ types.rkt
@@ -1,29 +1,24 @@
 #lang racket
 
-(provide llvm-int32-type
-         llvm-int64-type
-         llvm-float-type
-         llvm-double-type
-         llvm-function-type
-         llvm-create-generic-value-of-int
-         llvm-create-generic-value-of-float
-         llvm-generic-value-to-int
-         llvm-generic-value-to-float)
+(provide (all-defined-out))
 
 (require ffi/unsafe
          "definer.rkt")
 
-(define-llvm llvm-int32-type (_fun -> _LLVMTypeRef)
-  #:c-id LLVMInt32Type)
+(define-llvm llvm-int-type (_fun _int -> _LLVMTypeRef) #:c-id LLVMIntType)
+(define-llvm llvm-int8-type (_fun -> _LLVMTypeRef) #:c-id LLVMInt8Type)
+(define-llvm llvm-int16-type (_fun -> _LLVMTypeRef) #:c-id LLVMInt16Type)
+(define-llvm llvm-int32-type (_fun -> _LLVMTypeRef) #:c-id LLVMInt32Type)
+(define-llvm llvm-int64-type (_fun -> _LLVMTypeRef) #:c-id LLVMInt64Type)
+(define-llvm llvm-int128-type (_fun -> _LLVMTypeRef) #:c-id LLVMInt128Type)
 
-(define-llvm llvm-int64-type (_fun -> _LLVMTypeRef)
-  #:c-id LLVMInt64Type)
-
-(define-llvm llvm-float-type (_fun -> _LLVMTypeRef)
-  #:c-id LLVMFloatType)
-
-(define-llvm llvm-double-type (_fun -> _LLVMTypeRef)
-  #:c-id LLVMDoubleType)
+(define-llvm llvm-half-type (_fun -> _LLVMTypeRef) #:c-id LLVMHalfType)
+(define-llvm llvm-bfloat-type (_fun -> _LLVMTypeRef) #:c-id LLVMBFloatType)
+(define-llvm llvm-float-type (_fun -> _LLVMTypeRef) #:c-id LLVMFloatType)
+(define-llvm llvm-double-type (_fun -> _LLVMTypeRef) #:c-id LLVMDoubleType)
+(define-llvm llvm-x86fp80-type (_fun -> _LLVMTypeRef) #:c-id LLVMX86FP80Type)
+(define-llvm llvm-fp128-type (_fun -> _LLVMTypeRef) #:c-id LLVMFP128Type)
+(define-llvm llvm-ppcfp128-type (_fun -> _LLVMTypeRef) #:c-id LLVMPPCFP128Type)
 
 (define-llvm llvm-function-type (_fun  (return-type [param-types (list)] [variadic? #f]) ::
                                        (return-type : _LLVMTypeRef)
