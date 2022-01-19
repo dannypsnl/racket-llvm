e99e3c9f6e4efd4c068c14b123624b4bc828d8b8
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-19 10:00:45 +0800

    use at-exp

--- definer.rkt
+++ definer.rkt
@@ -1,4 +1,4 @@
-#lang racket
+#lang at-exp racket/base
 
 (provide define-llvm
          _LLVMModuleRef
@@ -12,8 +12,12 @@
          _LLVMPassManagerBuilderRef)
 
 (require ffi/unsafe
-         ffi/unsafe/define)
-(require scribble/srcdoc (for-doc scribble/manual ffi/unsafe racket/base))
+         ffi/unsafe/define
+         racket/string
+         racket/port
+         racket/system)
+(require scribble/srcdoc
+         (for-doc scribble/manual ffi/unsafe racket/base))
 
 (define (get-llvm-lib-dir)
   (let ([path (find-executable-path "llvm-config")])
@@ -21,11 +25,10 @@
            (with-output-to-string
              (λ () (system* path "--libdir")))))))
 
-(provide (form-doc (define-llvm id function-type #:c-id c-function-id)
-                   ((racket define-llvm)
-                    " takes an id as its produces function name, then is a C " (racket _fun) " defined types for C function. Finally, takes the link name of C function."
-                    " "
-                    "Using " (racket define-llvm) " to extend any functions missing in this library will be important.")))
+(provide
+ (form-doc
+  (define-llvm id function-type #:c-id c-function-id)
+  @{@racket[define-llvm] takes an id as its produces function name, then is a C @racket[_fun] defined types for C function. Finally, takes the link name of C function. Using @racket[define-llvm] to extend any functions missing in this library will be important.}))
 (define-ffi-definer define-llvm (ffi-lib "libLLVM" '("13" "12" "11" #f)
                                          #:get-lib-dirs get-llvm-lib-dir))
 
--- info.rkt
+++ info.rkt
@@ -1,7 +1,10 @@
 #lang info
 (define collection "racket-llvm")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib"
+                     "racket-doc"
+                     "rackunit-lib"
+                     "at-exp-lib"))
 (define scribblings '(("scribblings/racket-llvm.scrbl" ())))
 (define pkg-desc "LLVM C-API bindings for Racket")
 (define version "0.0")
