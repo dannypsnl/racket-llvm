730cc233cc47587486fe70c172b7463c19f6ce8e
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-25 09:15:24 +0800

    doc done
    close #1

--- ref.rkt
+++ ref.rkt
@@ -1,28 +1,25 @@
 #lang at-exp racket/base
-(provide LLVMModuleRef?
-         LLVMContextRef?
-         LLVMTypeRef?
-         LLVMValueRef?
-         LLVMBasicBlockRef?
-         LLVMBuilderRef?
-         LLVMGenericValueRef?
-         LLVMPassManagerRef?
-         LLVMPassManagerBuilderRef?)
+(provide  _LLVMModuleRef
+          _LLVMContextRef
+          _LLVMTypeRef
+          _LLVMValueRef
+          _LLVMBasicBlockRef
+          _LLVMBuilderRef
+          _LLVMGenericValueRef
+          _LLVMPassManagerRef
+          _LLVMPassManagerBuilderRef
+          LLVMModuleRef?
+          LLVMContextRef?
+          LLVMTypeRef?
+          LLVMValueRef?
+          LLVMBasicBlockRef?
+          LLVMBuilderRef?
+          LLVMGenericValueRef?
+          LLVMPassManagerRef?
+          LLVMPassManagerBuilderRef?)
 
 (require ffi/unsafe)
-(require scribble/srcdoc
-         (for-doc scribble/manual ffi/unsafe racket/base))
 
-(provide
- (thing-doc _LLVMModuleRef ctype? @{pointer to LLVMOpaqueModule})
- (thing-doc _LLVMContextRef ctype? @{pointer to LLVMContextRef})
- (thing-doc _LLVMTypeRef ctype? @{pointer to LLVMOpaqueType})
- (thing-doc _LLVMValueRef ctype? @{pointer to LLVMOpaqueValue})
- (thing-doc _LLVMBasicBlockRef ctype? @{pointer to LLVMOpaqueBasicBlock})
- (thing-doc _LLVMBuilderRef ctype? @{pointer to LLVMOpaqueBuilder})
- (thing-doc _LLVMGenericValueRef ctype? @{pointer to LLVMOpaqueGenericValue})
- (thing-doc _LLVMPassManagerRef ctype? @{pointer to LLVMOpaquePassManager})
- (thing-doc _LLVMPassManagerBuilderRef ctype? @{pointer to LLVMOpaquePassManagerBuilder}))
 (define-cpointer-type _LLVMModuleRef #:tag 'LLVMOpaqueModule)
 (define-cpointer-type _LLVMContextRef #:tag 'LLVMContextRef)
 (define-cpointer-type _LLVMTypeRef #:tag 'LLVMOpaqueType)
--- scribblings/llvm-structure.scrbl
+++ scribblings/llvm-structure.scrbl
@@ -3,3 +3,76 @@
                     racket/base]]
 
 @title{LLVM Structure}
+
+@defthing[_LLVMModuleRef ctype?]{
+    pointer to LLVMOpaqueModule
+}
+@defproc[(LLVMModuleRef? [v any/c])
+    boolean?
+]{
+    check a value is LLVMOpaqueModule or not
+}
+@defthing[_LLVMContextRef ctype?]{
+    pointer to LLVMContextRef
+}
+@defproc[(LLVMContextRef? [v any/c])
+    boolean?
+]{
+    check a value is LLVMContextRef or not
+}
+@defthing[_LLVMTypeRef ctype?]{
+    pointer to LLVMOpaqueType
+}
+@defproc[(LLVMTypeRef? [v any/c])
+    boolean?
+]{
+    check a value is LLVMOpaqueType or not
+}
+@defthing[_LLVMValueRef ctype?]{
+    pointer to LLVMOpaqueValue
+}
+@defproc[(LLVMValueRef? [v any/c])
+    boolean?
+]{
+    check a value is LLVMOpaqueValue or not
+}
+@defthing[_LLVMBasicBlockRef ctype?]{
+    pointer to LLVMOpaqueBasicBlock
+}
+@defproc[(LLVMBasicBlockRef? [v any/c])
+    boolean?
+]{
+    check a value is LLVMOpaqueBasicBlock or not
+}
+@defthing[_LLVMBuilderRef ctype?]{
+    pointer to LLVMOpaqueBuilder
+}
+@defproc[(LLVMBuilderRef? [v any/c])
+    boolean?
+]{
+    check a value is LLVMOpaqueBuilder or not
+}
+@defthing[_LLVMGenericValueRef ctype?]{
+    pointer to LLVMOpaqueGenericValue
+}
+@defproc[(LLVMGenericValueRef? [v any/c])
+    boolean?
+]{
+    check a value is LLVMOpaqueGenericValue or not
+}
+@defthing[_LLVMPassManagerRef ctype?]{
+    pointer to LLVMOpaquePassManager
+}
+@defproc[(LLVMPassManagerRef? [v any/c])
+    boolean?
+]{
+    check a value is LLVMOpaquePassManager or not
+}
+@defthing[_LLVMPassManagerBuilderRef ctype?]{
+    pointer to LLVMOpaquePassManagerBuilder
+}
+@defproc[(LLVMPassManagerBuilderRef? [v any/c])
+    boolean?
+]{
+    check a value is LLVMOpaquePassManagerBuilder or not
+}
