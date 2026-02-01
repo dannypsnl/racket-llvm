1311a7fb93a5b2a402b5d41ba7543e1909e6d896
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2026-02-01 21:45:21 +0800

    version

--- info.rkt
+++ info.rkt
@@ -8,7 +8,7 @@
                      "at-exp-lib"))
 (define scribblings '(("scribblings/racket-llvm.scrbl" (multi-page))))
 (define pkg-desc "LLVM C-API bindings for Racket")
-(define version "0.0")
+(define version "1.0")
 (define license '(Apache-2.0 OR MIT))
 (define pkg-authors '(dannypsnl))
 
